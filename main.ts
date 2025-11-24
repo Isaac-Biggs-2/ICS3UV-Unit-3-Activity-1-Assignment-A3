db40c9f89e213c8fbfd9e7b5764efb20d45076bf
default <isaac.biggs@stu.ocsb.ca>
2025-11-24 21:49:14 +0000

    Created program to display multiplication table

---
+++ main.ts
@@ -0,0 +1,38 @@
+/**
+ * @author Isaac Biggs
+ * @version 1.0.0
+ * @date 2025-11-24
+ * @fileoverview List multiplication table of 9 from 1-12
+*/
+
+//variables
+const ZERO: number =  (0*9);
+const ONE: number =  (1*9);
+const TWO: number =  (2*9);
+const THREE: number = (3*9);
+const FOUR: number = (4*9);
+const FIVE: number = (5*9);
+const SIX: number = (6*9);
+const SEVEN: number = (7*9);
+const EIGHT: number = (8*9);
+const NINE: number = (9*9);
+const TEN: number = (10*9);
+const ELEVEN: number = (11*9);
+const TWELVE: number = (12*9);
+
+//display table to terminal
+console.log("0x9 = " + ZERO);
+console.log("1x9 = " + ONE);
+console.log("2x9 = " + TWO);
+console.log("3x9 = " + THREE);
+console.log("4x9 = " + FOUR);
+console.log("5x9 = " + FIVE);
+console.log("6x9 = " + SIX);
+console.log("7x9 = " + SEVEN);
+console.log("8x9 = " + EIGHT);
+console.log("9x9 = " + NINE);
+console.log("10x9 = " + TEN);
+console.log("11x9 = " + ELEVEN);
+console.log("12x9 = " + TWELVE);
+
+console.log("\nDone.")
